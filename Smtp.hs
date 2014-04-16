e4c1fb5f7816dd471ec7cee40b001a6392a1d0f1
Todd Lunter <tlunter@gmail.com>
2014-04-15 23:43:17 -0400

    Send email to IFTTT when given username, password and name

---
+++ Smtp.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Smtp (
+    sendMailForName
+) where
+
+import Network.Mail.Mime
+import qualified Network.Mail.Client.Gmail as G
+import qualified Data.Text.Lazy as T
+
+simpleMessage :: T.Text -> T.Text
+simpleMessage name = T.concat [name, " is here!"]
+
+sendMailForName :: T.Text -> T.Text -> T.Text -> IO ()
+sendMailForName username password name = G.sendGmail username password me [ifttt] [] [] "#dooralarm" (simpleMessage name) []
+
+me :: Address
+me = Address
+        { addressName = Just "Todd Lunter"
+        , addressEmail = "tlunter@gmail.com"
+        }
+
+ifttt :: Address
+ifttt = Address
+        { addressName = Nothing
+        , addressEmail = "trigger@ifttt.com"
+        }
