cc758b8e6bd0bdba6ba42c98e8484e3762154b5e
Todd Lunter <tlunter@gmail.com>
2014-04-18 17:12:28 -0400

    Delete a Mac through a different simple form

---
+++ Controllers/Delete.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Controllers.Delete (
+    deleteMac
+) where
+
+import Web.Scotty.Trans as S
+import Web.Scotty.Hastache
+import Control.Monad.IO.Class (liftIO)
+import qualified Database.Redis as R
+import qualified Data.Text.Lazy as T
+import qualified Data.ByteString as B
+
+import Util (convertTextToByteString)
+import Control.Monad (when, unless)
+
+deleteMac :: R.Connection -> ScottyH' ()
+deleteMac redisConn = post "/delete" $ do
+    textMac <- param "mac"
+
+    when ((T.length . T.strip $ textMac) < 17) (raise "Bad MAC")
+    
+    let keyToDelete = B.concat ["macs:", convertTextToByteString textMac]
+    value <- liftIO $ R.runRedis redisConn $ R.del [keyToDelete]
+    liftIO $ print value
+    either couldntDelete checkNumDeleted value
+    redirect "/"
+    `rescue`
+    text
+    where checkNumDeleted x = unless (x > 0) (couldntDelete ())
+          couldntDelete   _ = raise "Couldn't delete"
+
