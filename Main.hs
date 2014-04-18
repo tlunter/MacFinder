77f8045bf6cd4a69fbe80c9104bfb3e209b65549
Todd Lunter <tlunter@gmail.com>
2014-04-18 15:39:55 -0400

    Add a couple of packages, use Controllers

--- Main.hs
+++ Main.hs
@@ -1,20 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
---import Text.Hastache
-import Text.Hastache
-import Text.Hastache.Context
 import Web.Scotty.Trans as S
 import Web.Scotty.Hastache
+import Network.Wai.Middleware.Static (addBase, staticPolicy)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Control.Monad.IO.Class (liftIO)
-import qualified Data.ByteString as B
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text as T
 import qualified Database.Redis as R
 import Data.Maybe
 
-import Util (convertTextToByteString, convertByteStringToText)
-import Smtp (sendMailForName)
 import Config (getConfig, getUsername, getPassword, checkValid)
+import Controllers.Index (index)
+import Controllers.Lease (lease)
 
 main :: IO ()
 main = do
@@ -28,39 +26,11 @@ main = do
         password = LT.pack . T.unpack $ fromJust maybePassword
 
     scottyH' 3000 $ do
+      middleware $ staticPolicy (addBase "public")
+      middleware logStdoutDev
       setTemplatesDir "templates"
-      post "/lease" $ do
-        textMac <- param "mac"
-        let bsMac = convertTextToByteString textMac
+      setTemplateFileExt ".mustache"
 
-        value <- liftIO $ R.runRedis redisConn $ R.hgetall (B.concat ["macs:", bsMac])
+      lease redisConn username password
 
-        let name = getName value
-        maybe next (liftIO . sendMailForName username password) name
-
-        text "Success"
-
-      get "/" $ do
-        keys <- liftIO $ R.runRedis redisConn $ R.keys "macs:*"
-        values <- liftIO $ R.runRedis redisConn $ R.mget $ getKeys keys
-        setH "keys" $ MuList $ map (mkStrContext . mkListContext) $ zip (getKeys keys) (getValues values)
-        hastache "index.html"
-        where mkListContext (key, _) "key"     = MuVariable key
-              mkListContext (_, value) "value" = MuVariable value
-
-getName :: Either R.Reply [(B.ByteString, B.ByteString)] -> Maybe LT.Text
-getName (Left _)  = Nothing
-getName (Right x) = getName' x
-
-getName' :: [(B.ByteString, B.ByteString)] -> Maybe LT.Text
-getName' []              = Nothing
-getName' (("name", x):_) = Just $ convertByteStringToText x
-getName' (_:fields)      = getName' fields
-
-getKeys :: Either R.Reply [B.ByteString] -> [B.ByteString]
-getKeys (Left _)         = []
-getKeys (Right x)        = x
-
-getValues :: Either R.Reply [Maybe B.ByteString] -> [B.ByteString]
-getValues (Left _)  = []
-getValues (Right x) = catMaybes x
+      index redisConn
