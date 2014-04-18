935e71efbe537ba0e366f4d467690bd29e823642
Todd Lunter <tlunter@gmail.com>
2014-04-18 17:11:56 -0400

    Add deconstruct back to Redis syntax

--- Types.hs
+++ Types.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Types (
-    Mac,
+    Mac(Mac),
     name,
     mac,
-    newMac
+    newMac,
+    deconstructMac
 ) where
 
 import qualified Data.Text.Lazy as T
 import qualified Data.ByteString as B
-import Util (convertByteStringToText)
+import Util (convertByteStringToText, convertTextToByteString)
 
 data Mac = Mac { name :: T.Text
                , mac  :: T.Text
@@ -24,3 +25,6 @@ newMac xs = Just $ foldr fillMac emptyMac xs
     where fillMac ("name", val) struct = struct { name = convertByteStringToText val }
           fillMac ("mac",  val) struct = struct { mac = convertByteStringToText val }
           fillMac _ struct             = struct
+
+deconstructMac :: Mac -> (B.ByteString, [(B.ByteString, B.ByteString)])
+deconstructMac m = (B.concat ["macs:", convertTextToByteString . mac $ m], [("mac", convertTextToByteString . mac $ m), ("name", convertTextToByteString . name $ m)])
